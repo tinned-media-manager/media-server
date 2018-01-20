fcff3081f219d92dd6d9628b8ee9e0371337f721
cody <codyjeromegreen@gmail.com>
2018-01-20 10:40:54 -0800

    worked on file with Eric for some reason it didnt work on his computer but it did on mine so commiting from here

--- server.js
+++ server.js
@@ -14,12 +14,14 @@ const conString = process.env.API_URL;
 const DETAIL = process.env.API_URL_DETAIL;
 const IMG_URI = process.env.IMG_URI;
 const IMG_DEFAULT = process.env.IMG_DEFAULT;
+const api_key = process.env.api_key;
+// const URL_RECOMMENDED = process.env.URL_RECOMMENDED;
 
 const client = new pg.Client(conString);
 client.connect();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 // gets all info from all popular movies --> want to get the 4 most popular movies
@@ -27,10 +29,10 @@ app.get('/', (req, res) => {
   let url_popular = `https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key=${process.env.api_key}`;
   superAgent.get(url_popular)
     .then(data => {
-      let arrPopular = data.body.results.sort(function(a, b) {
+      let arrPopular = data.body.results.sort(function (a, b) {
         return b.popularity - a.popularity;
       }).filter((movie, index) => {
-        if (index < 4) {return movie;}
+        if (index < 4) { return movie; }
       });
       res.send(arrPopular);
       // console.log(data.body.results[0].title);
@@ -38,13 +40,36 @@ app.get('/', (req, res) => {
     }).catch(err => console.error(err));
 });
 
+
+// first time visiting webpage populated movies
+app.get('/recommend', (req, res) => {
+  let Url_Recommend = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=vote_count.desc&include_adult=false&include_video=false&page=1`
+  superAgent.get(Url_Recommend)
+    .then(data => {
+      let initialRecMovies = data.body.results.filter((movie, index) => {
+        if (index < 4) {return movie;}
+        console.log(movie);
+      });
+      res.send(initialRecMovies);
+    })
+    .catch(err => console.log(err));
+
+});
+
+
+
+
+
+
+
+
 // `${IMG_URI}${IMG_DEFAULT}${data.body.poster_path}">` this is the poster path figure out why this isnt working
 
 app.get('/one', (req, res) => {
   superAgent.get(DETAIL)
     .then(data => {
       console.log(data.body.adult);
-      res.send([data.body.title, data.body.overview, data.body.tagline, data.body.genres, ]);
+      res.send([data.body.title, data.body.overview, data.body.tagline, data.body.genres,]);
       err => res.send(err);
     });
 });
