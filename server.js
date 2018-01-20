5a2e746753392946e951cf6e60413d0a6baa3d2b
Chris Lesesne <clesesne3@gmail.com>
2018-01-20 11:41:42 -0800

    route to search by movie title works

--- server.js
+++ server.js
@@ -11,17 +11,22 @@ const app = express();
 
 require('dotenv').config();
 const conString = process.env.API_URL;
+const DETAIL = process.env.API_URL_DETAIL;
+const IMG_URI = process.env.IMG_URI;
+const IMG_DEFAULT = process.env.IMG_DEFAULT;
+const api_key = process.env.api_key;
+
 
 const client = new pg.Client(conString);
 client.connect();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 // gets the 4 most popular movies
 app.get('/api/movies/popular', (req, res) => {
-  let url_popular = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+  let url_popular = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
   superAgent.get(url_popular)
     .then(data => {
       let arrPopular = data.body.results.filter((movie, index) => {
@@ -33,7 +38,7 @@ app.get('/api/movies/popular', (req, res) => {
 
 // gets 4 recommended movies
 app.get('/api/movies/recommend', (req, res) => {
-  let url_recommend = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.api_key}&language=en-US&sort_by=vote_count.desc&include_adult=false&include_video=false&page=1`;
+  let url_recommend = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=vote_count.desc&include_adult=false&include_video=false&page=1`;
   superAgent.get(url_recommend)
     .then(data => {
       let arrRecommend = data.body.results.filter((movie, index) => {
@@ -44,16 +49,25 @@ app.get('/api/movies/recommend', (req, res) => {
     .catch(err => console.error(err));
 });
 
-
 // `${IMG_URI}${IMG_DEFAULT}${data.body.poster_path}">` this is the poster path figure out why this isnt working
 
-app.get('/one', (req, res) => {
+app.get('/api/movies/one', (req, res) => {
   superAgent.get(DETAIL)
     .then(data => {
       console.log(data.body.adult);
-      res.send([data.body.title, data.body.overview, data.body.tagline, data.body.genres, ]);
+      res.send([data.body.title, data.body.overview, data.body.tagline, data.body.genres,]);
       err => res.send(err);
     });
+});
+
+// gets movies based on user search by title
+app.get('/api/movies/:title', (req, res) => {
+  let url_search = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${req.params.title}`;
+  superAgent.get(url_search)
+    .then(data => {
+      res.send(data.body.results);
+    })
+    .catch(err => console.error(err));
 });
 
 app.listen(PORT, () => {
